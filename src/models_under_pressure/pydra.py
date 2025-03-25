b79046f2c3afa05a8e211dd13923c2a5bbd0fa64
Alex McKenzie <me+github@alexmck.com>
2025-03-25 16:23:37 +0000

    Make code nice and compact

--- src/models_under_pressure/pydra.py
+++ src/models_under_pressure/pydra.py
@@ -14,20 +14,11 @@ def main(*args: Any, **kwargs: Any) -> Callable:
     def decorator(func: Callable[..., T]) -> Callable[..., T]:
         @hydra.main(*args, **kwargs)
         @functools.wraps(func)
-        def wrapper(dict_config: DictConfig) -> T:
-            # Get the type annotation for the config parameter
-            type_hints = get_type_hints(func)
-            config_type = type_hints["config"]
-
-            # Convert DictConfig to a regular dictionary, handling nested DictConfigs
-            config_dict = OmegaConf.to_container(
-                dict_config, resolve=True, enum_to_str=True
-            )
-
-            # Create the typed config
-            typed_config = config_type.model_validate(config_dict)
-
-            return func(typed_config)
+        def wrapper(config: DictConfig) -> T:
+            config_type = get_type_hints(func)["config"]
+            config_dict = OmegaConf.to_container(config, resolve=True, enum_to_str=True)
+            config_model = config_type.model_validate(config_dict)
+            return func(config_model)
 
         return wrapper
 
